4b323362b8aac6927e72aba927cd0bb39544e23f
Amy Cherrez <146954336+Amy525-g@users.noreply.github.com>
2025-01-25 23:09:45 -0500

    frontend

--- @MiniCore/@MiniCore/core/src/main/java/com/minicore/core/repositories/DepartamentoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.minicore.core.repositories;
-
-import com.minicore.core.models.Departamento;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DepartamentoRepository extends JpaRepository<Departamento, Long> {
-}
